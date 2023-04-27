61203e83fb231b23c6264215b12eeeb39ddc3493
Karthik Garidepalli <58597785+Karthik651@users.noreply.github.com>
2023-04-27 01:18:56 -0500

    Update Bike_Rental_App.py

--- Bike_Rental_App.py
+++ Bike_Rental_App.py
@@ -1,11 +1,11 @@
-
 import tkinter as tk
 from tkinter import ttk
 import mysql.connector
-import pandas as pd
-from pandastable import Table
 
 def show_table_contents():
+    for record in tree.get_children():
+        tree.delete(record)
+
     table_name = table_var.get()
     
     if table_name:
@@ -13,14 +13,14 @@ def show_table_contents():
         cursor.execute(query)
         records = cursor.fetchall()
         column_names = [desc[0] for desc in cursor.description]
-        df = pd.DataFrame(records, columns=column_names)
 
-        table_frame = tk.Toplevel(root)
-        table_frame.title(f"Contents of {table_name}")
-        table_frame.geometry("800x400")
+        tree["columns"] = column_names
+        for col_name in column_names:
+            tree.column(col_name, anchor="w", stretch=tk.YES, minwidth=100)
+            tree.heading(col_name, text=col_name)
 
-        pt = Table(table_frame, dataframe=df)
-        pt.show()
+        for record in records:
+            tree.insert('', 'end', values=record)
 
 # Connect to the MySQL database
 db = mysql.connector.connect(
@@ -34,22 +34,35 @@ cursor = db.cursor()
 # Define the GUI
 root = tk.Tk()
 root.title("MySQL Table Viewer")
-root.geometry("300x200")
+root.geometry("800x400")
 
 table_var = tk.StringVar()
 
 label = ttk.Label(root, text="Select a table:")
-label.pack(pady=10)
+label.grid(row=0, column=0, pady=10, padx=10)
 
 table_combo = ttk.Combobox(root, textvariable=table_var)
 table_combo['values'] = ('customers', 'bikes', 'rentals', 'rental_return')  # Your table names
-table_combo.pack(pady=10)
+table_combo.grid(row=0, column=1, pady=10)
 
 show_button = ttk.Button(root, text="Show Table Contents", command=show_table_contents)
-show_button.pack(pady=10)
+show_button.grid(row=0, column=2, pady=10, padx=10)
+
+tree = ttk.Treeview(root, selectmode="browse")
+tree.grid(row=1, column=0, columnspan=3, padx=10, pady=10, sticky="nsew")
+
+# Add scrollbar
+scrollbar = ttk.Scrollbar(root, orient="vertical", command=tree.yview)
+scrollbar.grid(row=1, column=3, padx=0, pady=10, sticky="nsew")
+tree.configure(yscrollcommand=scrollbar.set)
+
+root.grid_rowconfigure(1, weight=1)
+root.grid_columnconfigure(2, weight=1)
 
 root.mainloop()
 
 # Close database connection
 cursor.close()
 db.close()
+
+
