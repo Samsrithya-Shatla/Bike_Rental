963fc988fde29b217f7309bee3f68de66165b09c
Karthik Garidepalli <58597785+Karthik651@users.noreply.github.com>
2023-04-27 01:22:51 -0500

    Update Bike_Rental_App.py

--- Bike_Rental_App.py
+++ Bike_Rental_App.py
@@ -10,3 +10,12 @@ root.title("BavisRentals - Bstands for Bike")
 # Set up the grid layout
 root.columnconfigure(0, weight=1)
 root.rowconfigure(1, weight=1)
+
+top_frame = ttk.Frame(root)
+top_frame.grid(row=1, column=0, sticky="nsew", padx=10, pady=10)
+
+left_frame = ttk.Frame(top_frame)
+left_frame.grid(row=0, column=0, sticky="nsew")
+
+right_frame = ttk.Frame(top_frame)
+right_frame.grid(row=0, column=1, sticky="nsew")
